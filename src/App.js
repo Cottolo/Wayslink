f8b1d391b3c0dc3c4102b252fb8441b93a94ceac
unknown <rahmat9654@gmail.com>
2022-11-14 14:36:09 +0700

    hello

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react'
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import LandingPage from './pages/LandingPage/langdingpage';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Profile from "./pages/profile/profile";
+import MyLinks from "./pages/Mylinks/Mylinks";
+import InsertPage from "./pages/Insertpage/InsertPage";
+import PreviewLink from "./components/previewlink/PreviewLink";
+
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<LandingPage/>} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/myLinks" element={<MyLinks  />} />
+      <Route path="/insertLink/:template" element={<InsertPage />} />
+      <Route path="/wayslink/:unique_link" element={<PreviewLink  />} />
+    </Routes>
   );
 }
 
