a99cdb94bce2c3c5f15ab1f7b847572c2e9b4fbd
unknown <rahmat9654@gmail.com>
2022-11-16 22:05:41 +0700

    up

--- models/link.go
+++ models/link.go
@@ -1,17 +1,17 @@
 package models
 
 type Link struct {
-	ID          int                   `json:"id" gorm:"primary_key:auto_increment"`
-	Title       string                `json:"title" gorm:"type:varchar(255)"`
-	Description string                `json:"description" gorm:"type:varchar(255)"`
-	UniqueLink  string                `json:"unique_link"`
-	Image       string                `json:"image" gorm:"type:varchar(255)"`
-	Template    string                `json:"template"`
-	Visit       int                   `json:"visit"`
-	Socmed_id   int                   `json:"socmed_id"`
-	SocialMedia []SocialMediaResponse `json:"social_media" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	UserID      int                   `json:"user_id"`
-	User        UserResponse          `json:"user" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	ID            int                   `json:"id" gorm:"primary_key:auto_increment"`
+	Title         string                `json:"title" gorm:"type:varchar(255)"`
+	Description   string                `json:"description" gorm:"type:varchar(255)"`
+	UniqueLink    string                `json:"unique_link"`
+	Image         string                `json:"image" gorm:"type:varchar(255)"`
+	Template      string                `json:"template"`
+	Visit         int                   `json:"visit"`
+	SocialMedia   []SocialMediaResponse `json:"social_media" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	SocialMediaID int                   `json:"social_media-id"`
+	UserID        int                   `json:"user_id"`
+	User          UserResponse          `json:"user" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 type LinkResponse struct {
--- models/user.go
+++ models/user.go
@@ -5,8 +5,8 @@ type User struct {
 	FullName string         `json:"full_name" gorm:"type:varchar(255)"`
 	Email    string         `json:"email" gorm:"type:varchar(255)"`
 	Password string         `json:"-" gorm:"type:varchar(255)"`
-	LinkID   int            `json:"link_id"`
 	Link     []LinkResponse `json:"link" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	LinkID   int            `json:"link_id"`
 }
 
 type UserResponse struct {
