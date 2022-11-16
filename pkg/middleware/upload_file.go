ba023ec079d30f7d794a76707327afdd1c6096c9
unknown <rahmat9654@gmail.com>
2022-11-16 20:16:36 +0700

    up

--- pkg/middleware/upload_file.go
+++ pkg/middleware/upload_file.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	dto "server/dto/result"
 )
 
 func UploadFile(next http.HandlerFunc) http.HandlerFunc {
@@ -13,12 +15,45 @@ func UploadFile(next http.HandlerFunc) http.HandlerFunc {
 
 		file, _, err := r.FormFile("image")
 
+		if err != nil && r.Method == "PATCH" {
+			ctx := context.WithValue(r.Context(), "dataFile", "false")
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
+		}
+
 		if err != nil {
-			fmt.Println(err)
-			json.NewEncoder(w).Encode("Error Retrieving the File")
+			w.WriteHeader(http.StatusBadRequest)
+			response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
+			json.NewEncoder(w).Encode(response)
 			return
 		}
 		defer file.Close()
+
+		// setup file type filtering
+		buff := make([]byte, 512)
+		_, err = file.Read(buff)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		filetype := http.DetectContentType(buff)
+		if filetype != "image/jpeg" && filetype != "image/png" {
+			w.WriteHeader(http.StatusBadRequest)
+			response := dto.ErrorResult{Code: http.StatusBadRequest, Message: "The provided file format is not allowed. Please upload a JPEG or PNG image"}
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		_, err = file.Seek(0, io.SeekStart)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+			json.NewEncoder(w).Encode(response)
+			return
+		}
 
 		const MAX_UPLOAD_SIZE = 10 << 20
 
