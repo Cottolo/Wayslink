75325a01de2f0dfb9458e04dc6248c10044d015e
unknown <rahmat9654@gmail.com>
2022-11-16 19:31:05 +0700

    update

--- src/components/modal/Deletemodal.jsx
+++ src/components/modal/Deletemodal.jsx
@@ -10,7 +10,7 @@ export default function DeleteData({ show, handleClose, setConfirmDelete }) {
         <Modal show={show} onHide={handleClose} centered>
             <Modal.Body className="text-dark">
                 <div style={{fontSize: '16px', fontWeight: '500'}} className="mt-2">
-                    Are you sure you want to delete this Link?
+                    Are you sure to delete this Link?
                 </div>
                 <div className="text-end mt-5">
                     <Button onClick={handleDelete} size="sm" className="btn-danger me-2" style={{width: '135px'}}>Yes</Button>
